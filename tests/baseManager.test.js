40dde49cd726bf39616cbd0aa2d5ca3f2c47d11d
OnafMadrevaucos <98037302+OnafMadrevaucos@users.noreply.github.com>
2025-02-04 09:59:47 -0300

    Delete Unused Tests

--- tests/baseManager.test.js
+++
@@ -1,9 +0,0 @@
-const baseManager = require('../src/scripts/managers/baseManager');
-
-test('baseManager should do something', () => {
-	expect(baseManager.someFunction()).toBe(someExpectedValue);
-});
-
-test('baseManager should handle errors', () => {
-	expect(() => baseManager.someFunctionThatThrows()).toThrow();
-});
